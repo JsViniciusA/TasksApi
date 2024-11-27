5f993d30bdeb0f0db337f4c835b7b8bb347c81a4
imalphabetboy <lucianotomechaves@gmail.com>
2024-11-27 20:00:06 +0000

    atualização

--- tasksapi/index.js
+++ tasksapi/index.js
@@ -1,8 +1,16 @@
-import express from "express";
+import { Sequelize } from 'sequelize';
+import config from '../config/config.json';
 import routes from "./src/routers/routes.js";
 
-
-
+const sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+  
+  const db = { sequelize, Sequelize };
+  
 /* GET home page. */
 routes.get("/", function (req, res, next) {
     res.status(200).send("Ok").end();
